5c61b43460c2e669b8b392b8927002963d9b0b96
saahilh <saahil_hamayun@yahoo.com>
2020-02-07 09:49:55 -0500

    Fixed cleanup logic

--- dist/column-resizer.js
+++ dist/column-resizer.js
@@ -1,188 +1,83 @@
-"use strict";
+import React, {useEffect, useRef} from 'react';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
+function ColumnResizer({disabled, index, refs, setRefs}) {
+  const resizer = useRef();
 
-var _react = _interopRequireDefault(require("react"));
+  const startDrag = (e) => {
+    const startPos = e.touches ? e.touches[0].screenX : e.screenX;
+    const previousSiblingWidth = parseInt(resizer.current.previousSibling.style.width);
+    const mouseMoveHandler = onMouseMove(previousSiblingWidth, startPos);
 
-var _propTypes = require("prop-types");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-var ColumnResizer =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(ColumnResizer, _React$Component);
-
-  function ColumnResizer(props) {
-    var _this;
-
-    _classCallCheck(this, ColumnResizer);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(ColumnResizer).call(this, props));
-    _this.startDrag = _this.startDrag.bind(_assertThisInitialized(_assertThisInitialized(_this)));
-    _this.endDrag = _this.endDrag.bind(_assertThisInitialized(_assertThisInitialized(_this)));
-    _this.onMouseMove = _this.onMouseMove.bind(_assertThisInitialized(_assertThisInitialized(_this)));
-
-    if (props.disabled) {
-      return _possibleConstructorReturn(_this);
-    }
-
-    _this.dragging = false;
-    _this.mouseX = 0;
-    _this.startPos = 0;
-    _this.startWidthPrev = 0;
-    _this.startWidthNext = 0;
-    return _this;
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', function() {
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', this)
+    });
   }
 
-  _createClass(ColumnResizer, [{
-    key: "startDrag",
-    value: function startDrag() {
-      if (this.props.disabled) {
-        return;
-      }
+  const onMouseMove = (originalWidth, startPos) => e => {
+    const currentElem = resizer.current;
+    const mouseX = e.touches ? e.touches[0].screenX : e.screenX;
 
-      this.dragging = true;
-      this.startPos = this.mouseX;
-      this.startWidthPrev = 0;
-      this.startWidthNext = 0;
+    const delta = startPos - mouseX;
+    const newWidth =  originalWidth - delta;
 
-      if (this.refs.ele) {
-        var prevSibling = this.refs.ele.previousSibling;
-        var nextSibling = this.refs.ele.nextSibling;
-
-        if (prevSibling) {
-          this.startWidthPrev = prevSibling.clientWidth;
-        }
-
-        if (nextSibling) {
-          this.startWidthNext = nextSibling.clientWidth;
-        }
-      }
+    if(newWidth < parseInt(currentElem.previousSibling.style.minWidth)||
+      newWidth > parseInt(currentElem.previousSibling.style.maxWidth)) {
+      return;
     }
-  }, {
-    key: "endDrag",
-    value: function endDrag() {
-      if (this.props.disabled) {
-        return;
-      }
 
-      this.dragging = false;
+    refs[index].forEach(ref => {
+      ref.current.previousSibling.style.width = newWidth + 'px';
+    });
+  }
+
+  useEffect(() => {
+    setRefs(prev => (
+      prev.map((refList, refIndex) => (
+        refIndex===index ? [...refList, resizer] : [...refList]
+      ))
+    ));
+
+    if(refs[index] && refs[index].length) {
+      resizer.current.previousSibling.style.width = refs[index][0].current.previousSibling.style.width;
     }
-  }, {
-    key: "onMouseMove",
-    value: function onMouseMove(e) {
-      if (this.props.disabled) {
-        return;
-      }
 
-      this.mouseX = e.touches ? e.touches[0].screenX : e.screenX;
-
-      if (!this.dragging) {
-        return;
-      }
-
-      var ele = this.refs.ele;
-      var moveDiff = this.startPos - this.mouseX;
-      var newPrev = this.startWidthPrev - moveDiff;
-      var newNext = this.startWidthNext + moveDiff;
-
-      if (newPrev < this.props.minWidth) {
-        var offset = newPrev - this.props.minWidth;
-        newPrev = this.props.minWidth;
-        newNext += offset;
-      } else if (newNext < this.props.minWidth) {
-        var _offset = newNext - this.props.minWidth;
-
-        newNext = this.props.minWidth;
-        newPrev += _offset;
-      }
-
-      ele.previousSibling.style.width = newPrev + 'px';
-      ele.nextSibling.style.width = newNext + 'px';
-    }
-  }, {
-    key: "componentDidMount",
-    value: function componentDidMount() {
-      if (this.props.disabled) {
-        return;
-      }
-
-      document.addEventListener('mousemove', this.onMouseMove);
-      document.addEventListener('mouseup', this.endDrag);
-      document.addEventListener("touchmove", this.onMouseMove);
-      document.addEventListener("touchend", this.endDrag);
-    }
-  }, {
-    key: "componentWillUnmount",
-    value: function componentWillUnmount() {
-      if (this.props.disabled) {
-        return;
-      }
-
-      document.removeEventListener('mousemove', this.onMouseMove);
-      document.removeEventListener('mouseup', this.endDrag);
-      document.removeEventListener('touchmove', this.onMouseMove);
-      document.removeEventListener('touchend', this.endDrag);
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var style = {
-        userSelect: "none"
-      };
-
-      if (!this.props.disabled) {
-        style.cursor = 'ew-resize';
-      }
-
-      if (this.props.className === "") {
-        style.width = '6px';
-        style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
-      }
-
-      return _react.default.createElement("td", {
-        ref: "ele",
-        style: style,
-        className: this.props.className,
-        onMouseDown: !this.props.disabled && this.startDrag,
-        onTouchStart: !this.props.disabled && this.startDrag
+    return function cleanup() {
+      setRefs(prev => {
+        const newPrev = prev.map((refList, refIndex) => {
+          if(refIndex===index) {
+            return refList.filter(elem => elem != resizer);
+          } else {
+            return refList;
+          }
+        })
+        return newPrev
       });
     }
-  }]);
+  }, []);
 
-  return ColumnResizer;
-}(_react.default.Component);
+  const style = {
+    WebkitTouchCallout: 'none',
+    WebkitUserSelect: 'none',
+    MozUserSelect: 'none',
+    MsUserSelect: 'none',
+    userSelect: 'none',
+    width: '6px',
+  }
 
-exports.default = ColumnResizer;
-ColumnResizer.defaultProps = {
-  disabled: false,
-  minWidth: 50,
-  className: ""
-};
-ColumnResizer.propTypes = {
-  disabled: _propTypes.bool,
-  minWidth: _propTypes.number,
-  className: _propTypes.string
-};
+  if (disabled) {
+    return <td ref={resizer} style={style} />
+  }
+
+  return (
+    <td
+      ref={resizer}
+      style={{...style, cursor: 'ew-resize'}}
+      onMouseDown={startDrag}
+      onTouchStart={startDrag} 
+    />
+  )
+}
+
+export default ColumnResizer;
